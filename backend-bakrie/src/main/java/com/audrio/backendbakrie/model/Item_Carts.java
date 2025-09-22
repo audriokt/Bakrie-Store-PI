96f854be980c6c9db19638061171d0dba1890e81
Diana Mahulae <dyanne886@gmail.com>
2025-09-22 19:43:49 +0700

    add entity

--- backend-bakrie/src/main/java/com/audrio/backendbakrie/model/Item_Carts.java
+++ backend-bakrie/src/main/java/com/audrio/backendbakrie/model/Item_Carts.java
@@ -1,4 +1,40 @@
 package com.audrio.backendbakrie.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.validator.constraints.UUID;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "item_carts")
+@Data
+
 public class Item_Carts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id_item_carts;
+
+    @ManyToOne
+    @JoinColumn(name = "id_cart")
+    private Carts id_cart;
+
+    @ManyToOne
+    @JoinColumn(name = "id_product")
+    private Products id_product;
+
+    @NotNull
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @NotNull
+    @Column(name = "added_at")
+    private LocalDateTime added_at;
+
+
+
+
+
+
+
 }
