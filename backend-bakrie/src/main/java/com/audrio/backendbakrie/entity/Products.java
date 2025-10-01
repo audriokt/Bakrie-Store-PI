e185ad17b5cde5a1b700cc81be6a587291415564
audrio <zerotwo8802@gmail.com>
2025-10-01 17:15:33 +0700

    add import

--- backend-bakrie/src/main/java/com/audrio/backendbakrie/entity/Products.java
+++ backend-bakrie/src/main/java/com/audrio/backendbakrie/entity/Products.java
@@ -8,7 +8,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.sql.Timestamp;
 import java.util.UUID;
 
 @Entity
