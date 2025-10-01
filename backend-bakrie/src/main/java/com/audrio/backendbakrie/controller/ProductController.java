7d053a785e6e123696f4ff040189a6bb0bec9955
audrio <zerotwo8802@gmail.com>
2025-10-01 14:28:29 +0700

    add product controller and service class

--- backend-bakrie/src/main/java/com/audrio/backendbakrie/controller/ProductController.java
+++ backend-bakrie/src/main/java/com/audrio/backendbakrie/controller/ProductController.java
@@ -1,4 +1,43 @@
 package com.audrio.backendbakrie.controller;
 
+import com.audrio.backendbakrie.io.ProductRequest;
+import com.audrio.backendbakrie.io.ProductResponse;
+import com.audrio.backendbakrie.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.lang.module.ResolutionException;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/products")
+@AllArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse addProduct(@RequestBody ProductRequest request){
+        return productService.add(request);
+    }
+
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> fetchAllProducts(){
+        return productService.getAll();
+    }
+
+    @DeleteMapping("/{productId}")
+    public void delete(@PathVariable String productId){
+        try {
+            productService.delete(UUID.fromString(productId));
+        } catch (Exception e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+
+    }
 }
