01b21e9d84ae88e108a32c7dc56759f0ecdcea81
zwitaagustina <zwitaagustinasianturi3536@gmail.com>
2025-10-02 20:04:14 +0700

    add repository class for order

---
+++ backend-bakrie/src/main/java/com/audrio/backendbakrie/Repository/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.audrio.backendbakrie.Repository;
+
+import com.audrio.backendbakrie.entity.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Orders, UUID> {
+    Optional<Orders> findByOrderNumber(String orderNumber);
+}
