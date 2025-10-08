57531bda41aaaf6f88f85c2945e2c07baef6f8c2
prisscillass <priscillasudiyantoro@gmail.com>
2025-10-08 18:02:59 +0700

    add new pages and add the route

--- frontend-bakrie/src/App.jsx
+++ frontend-bakrie/src/App.jsx
@@ -1,6 +1,9 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/layout/footer/footer'
 import Navbar from './components/layout/navbar/Navbar'
+import HomePage from './components/pages/home/HomePage'
+import AboutPage from './components/pages/about/AboutPage'
 
 
 
@@ -8,7 +11,10 @@ function App() {
   return (
     <>
       <Navbar />
-      <p>halotecbcw</p>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
       <Footer />
     </>
   )
