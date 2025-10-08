fb0d50cddfe2bca72092dea0e36b7b7f1c65d70e
prisscillass <priscillasudiyantoro@gmail.com>
2025-10-08 17:51:11 +0700

    editing navbar and footer

--- frontend-bakrie/src/components/layout/footer/Footer.jsx
+++ frontend-bakrie/src/components/layout/footer/Footer.jsx
@@ -4,9 +4,9 @@ const Footer = () => {
       <div>
         <img src="./logo/Patteserie.svg" alt="Patteserie" className="w-48" />
         {/* Copyright */}
-        <div className="flex items-center gap-[2px] text-yes px-11">
+        <div className="flex items-center gap-[2px] text-yes px-12">
           <span className="text-sm md:text-xl">&copy;</span>
-          <p className="text-[10px] md:text-base font-semibold pt-[2px]">
+          <p className="text-[10px] font-semibold pt-[2px]">
             2025 Patteserie
           </p>
         </div>
--- frontend-bakrie/src/components/layout/navbar/Navbar.jsx
+++ frontend-bakrie/src/components/layout/navbar/Navbar.jsx
@@ -1,27 +1,49 @@
 import { useState } from "react"
+import { motion } from "framer-motion"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const handleClick = () => {
     setIsOpen(!isOpen);
   }
+
   return (
-    <nav className="flex justify-between items-center w-screen h-28 bg-ookay margin-0 px-10 auto relative">
+    <nav className="flex justify-between items-center w-screen h-28 bg-none margin-0 px-10 auto relative">
       {/* Hamburger Menu */}
       <button className="flex justify-center items-center gap-2 w-auto relative z-50" onClick={ handleClick }>
         <div className="flex flex-col gap-1">
-          <span className="w-7 h-[3px] bg-yes rounded-sm"></span>
-          <span className="w-6 h-[3px] bg-yes rounded-sm"></span>
-          <span className="w-7 h-[3px] bg-yes rounded-sm"></span>
+          <span className={`w-7 h-[3px] bg-yes rounded-s transition-transform duration-300 ${isOpen && "rotate-45 translate-y-2"}`}></span>
+          <span className={`w-7 h-[3px] bg-yes rounded-s transition-[width] ${isOpen && "w-auto m-auto"}`}></span>
+          <span className={`w-7 h-[3px] bg-yes rounded-s transition-transform duration-300 ${isOpen && "-rotate-45 -translate-y-1.5"}`}></span>
         </div>
       </button>
       <img src="./logo/Patteserie.svg" alt="Patteserie" className="w-48" />
       <a href="/" className=""><i className="bx bx-shopping-bag text-2xl text-yes"></i></a>
       
-      {isOpen && <div className="absolute top-0 left-0 min-h-screen min-w-80 bg-blue-500 transition-all duration-300 ease-out">
-        hello
-      </div>}
+      {/* sidebar menu section */}
+      {isOpen && (
+        <>
+          <motion.div 
+            initial={{ x: "-100%" }}
+            whileInView={{ x: 0 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            className="absolute top-0 left-0 min-h-screen min-w-80 bg-ookay z-40">
+            <div className="flex flex-col justify-center absolute left-10 pt-28 gap-10 text-lg font-semibold text-yes">
+              {/* sign in */}
+              <Link to="/">Home</Link>
+              <Link to="/products">Products</Link>
+              <Link to="/about">About Us</Link>
+              {/* <a href="/"><p>Home</p></a>
+              <a href="/products"><p>Products</p></a>
+              <a href="/about"><p>About Us</p></a> */}
+            </div>
+          </motion.div> 
 
+          {/* Darken overlay */}
+          <motion.div transition={{ duration: 0.5, ease: "easeInOut" }} className="fixed inset-0 bg-opacity-15 backdrop-blur-[2px]" onClick={handleClick}></motion.div>
+        </>
+      )}
     </nav>
   )
 }
