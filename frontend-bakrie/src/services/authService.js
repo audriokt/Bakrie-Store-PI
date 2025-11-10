7bb926d484a47f07c1bd881ab10688f53321f452
audrio <zerotwo8802@gmail.com>
2025-11-10 18:15:53 +0700

    add header in API return value ini authService.js dan customerService.js

--- frontend-bakrie/src/services/authService.js
+++ frontend-bakrie/src/services/authService.js
@@ -3,5 +3,7 @@ import axios from 'axios'
 const URL_BASE = "http://localhost:9090/api/v1.0"
 
 export const loginCustomer = async (data) => {
-    return await axios.post(`${URL_BASE}/public/auth/login/customer`, data)
+    return await axios.post(`${URL_BASE}/public/auth/login/customer`, data, {
+        header: {'Authorization': `Bearer ${localStorage.getItem('token')}`}
+    })
 }
--- frontend-bakrie/src/services/customerService.js
+++ frontend-bakrie/src/services/customerService.js
@@ -1,3 +1,23 @@
 import axios from 'axios'
 
 const URL_BASE = "http://localhost:9090/api/v1.0"
+
+export const addCustomer = async (data) => {
+    return await axios.post(`${URL_BASE}/public/auth/register/customer`, data, {
+        headers: {'Authorization': `Bearer ${localStorage.getItem('token')}`}
+    })
+}
+
+export const deleteUser = async (userId) => {
+    return await axios.delete(`${URL_BASE}/customer/delete/${userId}`, {
+        header: {'Authorization': `Bearer ${localStorage.getItem('token')}`}
+    })
+}
+
+export const fetchCustomers = async () => {
+    return await axios.get(`${URL_BASE}/admin/customers/fetchCustomers`, {
+        header: {'Authorization': `Bearer ${localStorage.getItem('token')}`}
+    })
+}
+
+
